6daa824be8303a2f2963dcfa66676b49bb7971bc
MathewsOwen <matheusowenowen@gmail.com>
2026-03-16 17:05:20 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,35 +1,53 @@
 const NEWS_API_KEY = "36f6190c46284b89876d032af65ecbf8";
 
 async function fetchJson(url) {
-  const response = await fetch(url, { cache: "no-store" });
+  const response = await fetch(url, {
+    cache: "no-store"
+  });
 
   if (!response.ok) {
-    throw new Error(`Erro na requisição: ${response.status}`);
+    throw new Error("Erro ao buscar dados");
   }
 
   return response.json();
 }
 
+/* =======================
+CRYPTO API
+======================= */
+
 async function fetchCryptoPrices() {
-  return fetchJson(
-    "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana,binancecoin,ripple&vs_currencies=usd"
-  );
+
+  const url =
+  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana,binancecoin,ripple&vs_currencies=usd";
+
+  return fetchJson(url);
 }
 
+/* =======================
+AÇÕES BRASIL
+======================= */
+
 async function fetchStocks() {
-  const data = await fetchJson(
-    "https://brapi.dev/api/quote/PETR4,VALE3,ITUB4,WEGE3,BBAS3,BBDC4,MGLU3,ELET3"
-  );
+
+  const url =
+  "https://brapi.dev/api/quote/PETR4,VALE3,ITUB4,WEGE3,BBAS3,BBDC4,MGLU3,ELET3";
+
+  const data = await fetchJson(url);
 
   return data.results || [];
 }
 
-async function fetchMarketNews() {
-  const query = "bolsa OR bitcoin OR mercado financeiro OR ibovespa OR ações";
+/* =======================
+NEWS API
+======================= */
 
-  return fetchJson(
-    `https://newsapi.org/v2/everything?q=${encodeURIComponent(
-      query
-    )}&language=pt&sortBy=publishedAt&pageSize=8&apiKey=${NEWS_API_KEY}`
-  );
+async function fetchMarketNews() {
+
+  const query = "bolsa OR bitcoin OR mercado financeiro OR ibovespa";
+
+  const url =
+  `https://newsapi.org/v2/everything?q=${encodeURIComponent(query)}&language=pt&sortBy=publishedAt&pageSize=8&apiKey=${NEWS_API_KEY}`;
+
+  return fetchJson(url);
 }
