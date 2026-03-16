d8a61844df638dcbfea19a17a2e6aad22bd1404c
MathewsOwen <matheusowenowen@gmail.com>
2026-03-16 11:14:45 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,15 @@
+async function loadCrypto(){
 
+const url="https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd"
+
+const response=await fetch(url)
+
+const data=await response.json()
+
+document.getElementById("btcPrice").innerText="$"+data.bitcoin.usd
+
+document.getElementById("ethPrice").innerText="$"+data.ethereum.usd
+
+}
+
+loadCrypto()
