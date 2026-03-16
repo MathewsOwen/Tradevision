fd56caf8534b98428a27c27f78a2526bbea0a958
MathewsOwen <matheusowenowen@gmail.com>
2026-03-16 11:38:02 -0300

    Update crypto.js

--- js/crypto.js
+++ js/crypto.js
@@ -1,1 +1,16 @@
+async function loadCrypto(){
+
+const url="https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana&vs_currencies=usd"
+
+const res=await fetch(url)
+const data=await res.json()
+
+const container=document.getElementById("cryptoTable")
+
+container.innerHTML=`Bitcoin: $${data.bitcoin.usd}<br>
+Ethereum: $${data.ethereum.usd}<br>
+Solana: $${data.solana.usd}`
+}
+
+loadCrypto()
 
