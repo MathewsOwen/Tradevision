22c594c46fc2e98a6280f17b60bf02b807a84ca4
MathewsOwen <matheusowenowen@gmail.com>
2026-03-16 11:36:40 -0300

    Delete js/config.js

--- js/config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-    // Link da sua planilha do Google
-    SHEET_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR9hPdpNw-jrALbP32-Pgqhwe_X98uaZht0tjtZXidCFwiXvXTjbJc5diOaI270W8m_hyHYpd5xwlQl/pub?gid=0&single=true&output=csv',
-    IBOV_SYMBOL: 'BMFBOVESPA:IBOV',
-    SPLASH_DELAY: 3000 // Tempo da animação (3 seg)
-};
